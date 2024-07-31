ce8fce6141186f7f2b29312511e4f1212d2853f0
Leandro Tombetta <leandro.tombetta87@gmail.com>
2024-07-31 16:57:15 -0300

    primer version del proyecto

--- tup/settings.py
+++ tup/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-98l8sabplwtv^6tzly+7cn-l-6i46$-ljigu+&j0bas6gp%w%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
