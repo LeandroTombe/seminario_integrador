705766bcd8a6ea48fd5c92f74cb5f6686ba01f22
Leandro Tombetta <leandro.tombetta87@gmail.com>
2024-08-04 17:03:23 -0300

    modificacion en el usuario con relacion alumno

--- cuentas/resources.py
+++ cuentas/resources.py
@@ -1,49 +1,34 @@
-from import_export import resources,fields 
-from .models import User
-from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
-from django.utils.crypto import get_random_string
-import environ
 from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+from import_export import resources, fields
+from estudiantes.models import Alumno  # Asegúrate de importar tu modelo Alumno
 
 User = get_user_model()
 
-
-
-# Inicializa environ
-env = environ.Env(
-    # Establece valores por defecto y convierte a los tipos esperados
-    DEBUG=(bool, False)
-)
-# Lee el archivo .env
-environ.Env.read_env(env_file='.env')
-
-
 class UserResource(resources.ModelResource):
     password = fields.Field(attribute='password', column_name='password')
-    
-    
+    legajo = fields.Field(attribute='legajo', column_name='legajo')
+    nombre = fields.Field(attribute='nombre', column_name='nombre')
+    apellido = fields.Field(attribute='apellido', column_name='apellido')
+    telefono = fields.Field(attribute='telefono', column_name='telefono')
+    email = fields.Field(attribute='email', column_name='email')
+    dni = fields.Field(attribute='dni', column_name='dni')
+
     class Meta:
-         model = User
-         export_order = ( 'email', 'nombre', 'apellido', 'password')
-         
-    
+        model = User
+        export_order = ('email', 'nombre', 'apellido', 'password', 'legajo', 'telefono', 'dni')
+
     def before_import_row(self, row, **kwargs):
-        
         email = row.get('email')
-        
-        # Verificar si el usuario ya existe
+
         if User.objects.filter(email=email).exists():
-            # Marcar la fila para que no se importe
             raise ValueError(f"El usuario con el email {email} ya existe. La fila será ignorada.")
-        
-        
-        # Usar el email como contraseña
+
         password = row['email']
         row['password'] = password
         row['is_active'] = True
 
-        # Enviar el correo electrónico
         send_mail(
             'Tu nueva contraseña',
             f'Tu nueva contraseña es: {password}',
@@ -53,11 +38,22 @@ class UserResource(resources.ModelResource):
         )
 
     def after_import_row(self, row, row_result, **kwargs):
-        # Establecer la contraseña del usuario
         user = User.objects.get(email=row['email'])
         user.set_password(row['password'])
         user.save()
-        # Asignar el rol "Estudiantes"
+
         group, _ = Group.objects.get_or_create(name='Alumno')
         user.groups.add(group)
 
+        # Crear o actualizar el modelo Alumno
+        Alumno.objects.update_or_create(
+            user=user,
+            defaults={
+                'legajo': row.get('legajo'),
+                'nombre': row.get('nombre'),
+                'apellido': row.get('apellido'),
+                'telefono': row.get('telefono'),
+                'email': row.get('email'),
+                'dni': row.get('dni')
+            }
+        )
--- estudiantes/models.py
+++ estudiantes/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from .validators import validar_nombre
+from django.contrib.auth.models import User
+from django.conf import settings
+
 
 class Materia(models.Model):
     idMateria = models.IntegerField(primary_key=True)
@@ -19,6 +22,7 @@ class Cuota(models.Model):
         return f'Cuota {self.nroCuota} - Año {self.año}'
 
 class Alumno(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     legajo = models.IntegerField()
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
