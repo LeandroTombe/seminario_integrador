5bb2cedbf282c6644c120dbd09948c21533eb87e
Leandro <leandro.tombetta87@gmail.com>
2024-08-25 20:51:54 -0300

    logging sacado

--- tup/settings.py
+++ tup/settings.py
@@ -139,45 +139,6 @@ DATABASES = {
 }
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
-            'style': '{',
-        },
-        'simple': {
-            'format': '{levelname} {message}',
-            'style': '{',
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs', 'django_info.log'),
-            'formatter': 'verbose',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file', 'console'],
-            'level': 'INFO',  # Cambia el nivel a INFO para menos detalles
-            'propagate': True,
-        },
-        'estudiantes': {
-            'handlers': ['file', 'console'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-    },
-}
 AUTH_USER_MODEL="cuentas.User"
 
 REST_FRAMEWORK = {
