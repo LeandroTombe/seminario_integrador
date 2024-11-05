8e3949aa16778af9bb22798b5d6dc0af4b6318b0
Leandro Tombetta <leandro.tombetta87@gmail.com>
2024-11-05 01:11:09 -0300

    cambios en el docker ci

--- tup/settings.py
+++ tup/settings.py
@@ -160,7 +160,7 @@ DATABASES = {
         'NAME':  env('DB_NAME'),
         'USER':  env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
-        'HOST':  env('DB_HOST'),
+        'HOST':  env('DB_HOST', default='db'),
         'PORT': env('DB_PORT', default='3306'),
     }
 }
